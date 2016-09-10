9fd7639890fed35fbe3b8771c3a2dd2b5b75c9ec
shijatp <shijatpon@gmail.com>
2016-09-10 15:39:44 +0530

    half portion completed.

--- header.php
+++ header.php
@@ -12,3 +12,40 @@
 <link href="<?php echo ROOT_DIR ?>css/style.css" rel="stylesheet">
 </head>
 <body>
+<div class="container-fluid sec1">
+<div class="container sec1_head">
+<div class="row">
+<div class="col-md-5">
+<img class="img-responsive" src="image/1.png" />
+
+</div>
+<div class="col-md-7">
+
+<ul class="topnav" id="myTopnav">
+
+  <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">about us</a></li>
+        <li><a href="#">services </a></li>
+        <li><a href="#">product</a></li>
+        <li><a href="#">rental services</a></li>
+          <li><a href="#">contact us</a></li>
+ 
+  <li class="icon">
+    <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
+  </li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+</script>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+include('header.php');
+?>
+<div class="container-fluid sec1">
+<div class="row">
+<img class="img-responsive" src="image/2.png" />
+</div>
+</div>
+
+<div class="container-fluid sec2">
+<div class="container sec2_head">
+<div class="row">
+<div class="col-md-6">
+<img class="img-responsive" src="image/3.png" />
+</div>
+<div class="col-md-6">
+<h1>ABOUT US</h1>
+<h3>jhgdjfbsdbfbsbfjjgbs jszdjkgkjsfkksgkg kjhjkdsfas jkjdakgj bjsdkjg jjbbjkasbkga bajbgjbakgg</h3>
+<p>ajfjsjfjadjdjbmbaBfabfbsdbf jjbsdkjs jbjbaf  kbdkbf affbakf afbkbh kbabfa bajkbf jbajfb bbjbadjk kbbvsbv jbakd kjbkab kbkbsa kjbkjbK kjkjK kjaFKBJf kjbkbdvksbb kzvkdz kjsKJv kjkjdv kjKV kjKJV jv<p>
+<button  type="button" class="btn btn-primary btn1">READ MORE</button>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid sec3">
+<div class="row">
+<img class="img-responsive" src="image/image4.png" />
+</div>
+<div class="container sec3_head">
+<h1>FEATURES</h1>
+<ul>
+<div class="col-md-4">
+<li>catachn plus air purifying hiter</li>
+<li>easiest to clean</li>
+<li>dot liquid crystal display</li>
+<li>excelent energy eficiancy</li>
+</div>
+<div class="col-md-4">
+
+<li>echo friendly operation</li>
+<li>24x/suport</li>
+<li>anti alergy enzyme filter </li>
+<li>qul test-9db</li>
+</div>
+<div class="col-md-4">
+
+<li>auto restart</li>
+<li>econo cool</li>
+<li>wide and long throw up to 12 metres</li>
+</div>
+</ul>
+</div>
+</div>
+
+
+<div class="container-fluid sec4">
+<img src="image/2.png" />
+</div>
+
+<div class="container-fluid sec5">
+<img src="image/1.png" />
+</div>
+<div class="container-fluid sec6">
+<div class="container sec6_head">
+<div class="col-md-3">
+<h5>ABOUT US</h5>
+<p>hhfhf jgjg ggu gjgu gg uhiuhi gugu guu gjg gg gyg ggg gg gg gg gg g gygygyg gygygf fff ff fftf tftftf tftft ttftff tftf fftf tft
+chgchgh hhhg ghhg jgjg gjyg jgg jgg jgjg jgg jgjg</p>
+</div>
+<div class="col-md-3">
+
+<h5>BENEFITS</h5>
+</div>
+<div class="col-md-3">
+
+<h5>OUR SERVICES</h5>
+</div>
+<div class="col-md-3">
+
+<h5>CONTACT INFO</h5>
+</div>
+
+
+</div>
+</div>
+
+
+
+
+
+
+
