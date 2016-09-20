220d4ff9aa52639173b596977eaa2e47f33f90e2
shijatp <shijatpon@gmail.com>
2016-09-20 18:18:48 +0530

    copleted

--- footer.php
+++ footer.php
@@ -1,6 +1,18 @@
 </body>
-<div class="container-fluid sec12">
-<div class="container">
+<div class="container-fluid sec7">
+<div class="container sec7_head">
+<div class="row">
+<div class="col-md-10">
+<h4>© 2016 - MITSUBISHI ELETRIC - All rights reserved</h4>
+</div>
+<div class="col-md-2">
+
+<img class="img-responsive" src="image/foot1.png" />
+<img class="img-responsive" src="image/foot2.png" />
+<img class="img-responsive" src="image/foot3.png" />
+<img class="img-responsive" src="image/foot4.png" />
+</div>
+</div>
 </div>
 </div>
 </html>
--- index.php
+++ index.php
@@ -1,84 +1,266 @@
 <?php
 include('header.php');
 ?>
-<div class="container-fluid sec1">
+
+
+<div class="container-fluid sec2">
 <div class="row">
-<img class="img-responsive" src="image/2.png" />
+<img class="img-responsive" src="image/banner.png" />
+</div>
+<div class="container sec2-head">
+<img  class="img-responsive" src="image/ac.png" />
+<h2>MITSUBISHI ELETRIC
+	</h2>
+	<button  type="button" class="button1">WELCOME TO</button>
 </div>
 </div>
 
-<div class="container-fluid sec2">
-<div class="container sec2_head">
+
+
+
+
+
+<div class="container-fluid sec3">
+<div class="container sec3_head">
 <div class="row">
 <div class="col-md-6">
-<img class="img-responsive" src="image/3.png" />
+<img class="img-responsive" src="image/sec2wmn.png" />
 </div>
 <div class="col-md-6">
 <h1>ABOUT US</h1>
-<h3>jhgdjfbsdbfbsbfjjgbs jszdjkgkjsfkksgkg kjhjkdsfas jkjdakgj bjsdkjg jjbbjkasbkga bajbgjbakgg</h3>
-<p>ajfjsjfjadjdjbmbaBfabfbsdbf jjbsdkjs jbjbaf  kbdkbf affbakf afbkbh kbabfa bajkbf jbajfb bbjbadjk kbbvsbv jbakd kjbkab kbkbsa kjbkjbK kjkjK kjaFKBJf kjbkbdvksbb kzvkdz kjsKJv kjkjdv kjKV kjKJV jv<p>
-<button  type="button" class="btn btn-primary btn1">READ MORE</button>
+<h3>
+Ut id nisl quis enim dignissim sagittis. Nam ipsum risus, rutrum vitae, vestibulum eu,lacus. </h3>
+<p>Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. 
+Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.
+ Vivamus elementum semper nisi.Fusce convallis metus id felis luctus adipiscing. 
+Nunc nec neque. Nulla porta dolor. Phasellus accumsan cursus velit. 
+Nam at tortor in tellus interdum sagittis.Vivamus laoreet.
+ Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus.
+  Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est.
+ Donec sodales sagittis magna. Nam pretium turpis et arcu.<p>
+<button  type="button" class="button2">READ MORE</button>
 </div>
 </div>
 </div>
 </div>
 
-<div class="container-fluid sec3">
+
+
+ <div class="container-fluid sec4">
 <div class="row">
-<img class="img-responsive" src="image/image4.png" />
-</div>
-<div class="container sec3_head">
+<div class="backimg">
+
 <h1>FEATURES</h1>
-<ul>
-<div class="col-md-4">
+ 
+<div class="container sec4_head">
+
+	<div class="list"> 
+    
+    
+     <div class="col-md-4">
+     <ul>
 <li>catachn plus air purifying hiter</li>
 <li>easiest to clean</li>
 <li>dot liquid crystal display</li>
 <li>excelent energy eficiancy</li>
-</div>
-<div class="col-md-4">
-
+</ul>
+     </div>
+     
+     <div class="col-md-4"><ul>
 <li>echo friendly operation</li>
 <li>24x/suport</li>
 <li>anti alergy enzyme filter </li>
-<li>qul test-9db</li>
-</div>
-<div class="col-md-4">
-
+<li>qul test-9db</li></ul>
+    </div>
+    
+    
+     <div class="col-md-4"> <ul>
 <li>auto restart</li>
 <li>econo cool</li>
-<li>wide and long throw up to 12 metres</li>
+<li>Catachinwide and long throw up to 12 metres</li></ul>
+     </div>
+ </div>
+ </div>
+ 
 </div>
+</div> 
+
+</div>
+
+
+
+
+
+
+</div>
+<style>
+.carousel{
+	}
+</style>
+<!-- start section6-->
+<div class="container-fluid sec5">
+<div class="container sec5_head">
+<div class="row">
+<div class="col-md-12">
+<div class="carousel slide" id="myCarousel">
+  <div class="carousel-inner">
+    <div class="item active">
+      <div class="col-xs-3"><a href="#"><img src="image/slide1.png" class="img-responsive" ></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide2.png"class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide3.png" class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide4.png" class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide1.png" class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide2.png" class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide3.png" class="img-responsive"></a></div>
+    </div>
+    <div class="item">
+      <div class="col-xs-3"><a href="#"><img src="image/slide4.png" class="img-responsive"></a></div>
+    </div>
+  </div>
+ 
+</div>
+</div>
+</div>
+
+</div>
+
+
+</div>
+
+<div class="container" style="position:relative">
+<div class="container">
+<div class="col-md-5">
+</div>
+<div class="col-md-1">
+
+<div class="row">
+<a class="left carousel-control" href="#myCarousel" data-slide="prev"><button  type="button" class="btn btn-primary btn1"> < </button>
+</a>
+<a class="right carousel-control" href="#myCarousel" data-slide="next"><button  type="button" class="btn btn-primary btn2"> > </button>
+</a>
+
+ </div>
+ </div>
+ <div class="col-md-6">
+</div>
+
+ </div>
+ </div>
+<!-- end section6-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br />
+<br />
+<div class="container-fluid sec6">
+<div class="container sec6_head">
+
+
+<div class="col-md-3">
+<div class="about">
+<h4>ABOUT US</h4>
+<p class="about1">
+Praesent vestibulum dapibus nibhunc egestas augue at pellentesque laoreet felis eros vehicula leo at malesuada velit leo quis pede. Praesent venenatis metus at tortor pulvinar varius nunc nec neque. Aliquam lorem antedsfdsf dap ibus in viverra quis feugiat a, tellus.</p>
+
+<p class="about2">Fusce vulputate eleifend sapienc ras dapibus. Praesent metus tellus element umeu semper adipiscin aesent</p>
+</div>
+</div>
+
+
+<div class="col-md-3">
+<div class="benefit">
+<h4>BENEFITS</h4>
+<p class="benefit1">Praesent vestibulum dap ibus nibhunc egestas</p>
+<p class="name1">By John Doe  Oct-15</p>
+<p class="benefit2">Praesent vestibulum dap ibus nibhunc egestas</p>
+<p class="name2" >By John Doe  Oct-15</p>
+<p class="benefit3">Praesent vestibulum dap ibus nibhunc egestas</p>
+<p class="name3">By John Doe  Oct-15</p>
+</div>
+</div>
+
+
+<div class="col-md-3">
+<div class="service">
+<h4>OUR SERVICES</h4>
+<ul>
+<li>Praesent vestibulum dap ibus</li>
+<li>Aliquam erat volutpat auctor</li>
+<li>Donec vitae orci sed dolor</li>
+<li>Praesent vestibulum dap ibus</li>
+<li>Class aptent taciti sociosqu</li>
 </ul>
 </div>
 </div>
 
 
-<div class="container-fluid sec4">
-<img src="image/2.png" />
-</div>
 
-<div class="container-fluid sec5">
-<img src="image/1.png" />
-</div>
-<div class="container-fluid sec6">
-<div class="container sec6_head">
 <div class="col-md-3">
-<h5>ABOUT US</h5>
-<p>hhfhf jgjg ggu gjgu gg uhiuhi gugu guu gjg gg gyg ggg gg gg gg gg g gygygyg gygygf fff ff fftf tftftf tftft ttftff tftf fftf tft
-chgchgh hhhg ghhg jgjg gjyg jgg jgg jgjg jgg jgjg sdgfh cvn dxhfg dsd fgffd</p>
+<div class="contact">
+<h4>CONTACT INFO</h4>
+<ul>
+<li class="one">New York, NY, United States</li>
+<li class="two">800 (123) 456 789</li>
+<li class="three">support@wptheme.com</li>
+<li class="four">www.waveonline.com</li>
+</ul>
 </div>
-<div class="col-md-3">
-
-<h5>BENEFITS</h5>
-</div>
-<div class="col-md-3">
-
-<h5>OUR SERVICES</h5>
-</div>
-<div class="col-md-3">
-
-<h5>CONTACT INFO</h5>
 </div>
 
 
@@ -86,8 +268,30 @@ chgchgh hhhg ghhg jgjg gjyg jgg jgg jgjg jgg jgjg sdgfh cvn dxhfg dsd fgffd</p>
 </div>
 
 
+<?php
+include('footer.php');
+?>
 
+<script>
 
+$('#myCarousel').carousel({
+  interval: 4000
+})
 
-
-
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+</script>
